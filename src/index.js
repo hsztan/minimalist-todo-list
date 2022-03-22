6d7cf0ac4ef65bbb9550563b1c9516308798c0f3
Henry Nawrocki <hdev@sztanski.com>
2022-03-22 12:14:01 -0500

    Add triple-dots elements in list items

--- src/index.js
+++ src/index.js
@@ -26,15 +26,27 @@ const tasks = [
   },
 ];
 
+const tripleDotHTML = `
+  <div id="triple-dot">
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+  </div>
+`;
+
 window.onload = () => {
   tasks.sort((a, b) => a.index - b.index);
   tasks.forEach((task) => {
     const listItem = document.createElement('li');
     listItem.classList.add('todo-item');
     const checkBox = document.createElement('input');
+    const textItem = document.createElement('p');
+    textItem.innerText = task.description;
     checkBox.type = 'checkbox';
+    checkBox.classList.add('status');
     listItem.appendChild(checkBox);
-    listItem.insertAdjacentText('beforeend', task.description);
+    listItem.appendChild(textItem);
+    listItem.insertAdjacentHTML('beforeend', tripleDotHTML);
     todoContainer.appendChild(listItem);
   });
 };
