a5927823822e8811534328d416dbc2d38a0ad8c6
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-03-30 16:27:05 -0400

    Finalize add-task test

--- crud.test.js
+++ crud.test.js
@@ -5,16 +5,25 @@ describe('createTask', () => {
   beforeAll(() => {
     document.body.innerHTML = fs.readFileSync('build/index.html');
   });
-  it('adds a new task object into the data array', () => {
+  beforeEach(() => {
     const newItemElem = document.getElementById('add-item');
     newItemElem.value = 'Aaron';
     const event = { constructor: { name: 'PointerEvent' } };
     createTask(event);
+  })
+  afterEach(() => {
+    tasks.splice(0);
+    localStorage.clear();
+  })
+  it('adds a new task object into the data array', () => {
     expect(tasks.length).toBe(1);
   });
   it('saves the task into the local storage', () => {
-    expect(1).toBe(1);
+    expect(localStorage['todo-tasks']).toBeTruthy();
   });
+  it('renders task on the DOM', () => {
+    expect(document.querySelector('.todo-item')).toBeTruthy();
+  })
 });
 
 describe('deleteAndRemoveTask', () => {
